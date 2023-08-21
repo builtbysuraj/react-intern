3ac6c5684a18f5b188e44ef6f030070b16e0a112
builtbysuraj <surajgupta3553@gmail.com>
2023-08-21 12:54:31 +0530

    feat: Finished Tree view of department

--- src/components/NestedTree.tsx
+++ src/components/NestedTree.tsx
@@ -1,16 +1,6 @@
 import data from "../data/data.json"
-import SingleItem from "./SingleItem"
+import TreeStructure from "./TreeStructure"
 
 export default function NestedTree() {
-  return (
-    <ul>
-      {data?.map((item) => (
-        <SingleItem
-          key={item.department}
-          department={item.department}
-          sub_departments={item.sub_departments}
-        />
-      ))}
-    </ul>
-  )
+  return <TreeStructure data={data} />
 }
