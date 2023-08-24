aba378cc2751b010bc5ed347e1f95ca0d94e3f07
builtbysuraj <surajgupta3553@gmail.com>
2023-08-24 13:54:30 +0530

    feat: Add Log out button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,25 @@
-import { Grid, Typography } from "@mui/material"
+import { Button, Grid, Typography } from "@mui/material"
+import { useNavigate } from "react-router-dom"
 import NestedTree from "../components/ParentTree"
 import PostList from "../components/PostList"
 
 export default function Home() {
+  const navigate = useNavigate()
   return (
     <Grid container spacing={3} m={2}>
       <Grid item xs={12}>
-        <Typography variant="h3" textAlign={"center"}>
+        <Typography variant="h3" textAlign={"center"} mx={5}>
           Welcome
+          <Button
+            onClick={() => {
+              localStorage.clear()
+              navigate("/login")
+            }}
+            variant="contained"
+            size="medium"
+          >
+            Log out
+          </Button>
         </Typography>
       </Grid>
       <Grid item xs={6}>
